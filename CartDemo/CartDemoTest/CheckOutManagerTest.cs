8358f8ef420cf1f42c60b4b8e26ed1edabbb47fa
snigdha1 <jyothikrishna.gummadi@tekzenit.com>
2016-12-25 14:35:49 -0600

    Test project for Cart Demo

---
+++ CartDemo/CartDemoTest/CheckOutManagerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CartDemoTest
+{
+    [TestClass]
+    public class CheckOutManagerTest
+    {
+        private CartDemo.CheckOutManager checkoutManager;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            //  TODO: Add test initialization code
+            checkoutManager = new CartDemo.CheckOutManager();
+        }
+
+
+        [TestMethod]
+        public void GetPriceForApplesWithBuyOneGetOneFreeOffer()
+        {
+            Assert.AreEqual(0.45, checkoutManager.CheckOut(new List<string> { "Apples" }));
+            Assert.AreEqual(0.45, checkoutManager.CheckOut(new List<string> { "Apples", "Apples" }));
+            Assert.AreEqual(0.9, checkoutManager.CheckOut(new List<string> { "Apples", "Apples", "Apples" }));
+            Assert.AreEqual(1.1, checkoutManager.CheckOut(new List<string> { "Apples", "Apples", "Oranges"}));
+            Assert.AreEqual(0, checkoutManager.CheckOut(new List<string> { "Grapes" }));
+
+        }
+    }
+}
